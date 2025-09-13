50bc65391f2ccbd8c6c46852090e0cb4d93df08a
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-12 22:27:35 -0500

    added concat

--- app.js
+++ app.js
@@ -9,33 +9,29 @@ onEvent("button2", "click", function () {
   var concat = getValue("usermessage");
   console.log(pluralNoun);
   console.log(noun1);
+  var userMessage =
+    "How to take care of your pet Emu.\n" +
+    "1. Make sure your Emu eats plenty of \n" +
+    pluralNoun +
+    ".\n" +
+    "2. Also, make sure your pet Emu gets plenty of \n" +
+    noun1 +
+    "\n as well." +
+    "\n" +
+    "3. Lastly, make sure your Emu has \n" +
+    noun2 +
+    "\n while" +
+    " \n" +
+    enterASong +
+    "\n" +
+    enterAVerb +
+    ".";
+  
+  setText("concat", userMessage);
 });
-var userMessage =
-  "How to take care of your pet Emu.\n" +
-  "1. Make sure your Emu eats plenty of \n" +
-  plural +
-  ".\n" +
-  "2. Also, make sure your pet Emu gets plenty of \n" +
-  nn1 +
-  "\n as well." +
-  "\n" +
-  "3. Lastly, make sure your Emu has \n" +
-  nn2 +
-  "\n while" +
-  " \n" +
-  sng +
-  "\n" +
-  vrb +
-  ".";
-setText("plural", plural);
-setText("nn1", nn1);
-setText("nn2", nn2);
-setText("sng", sng);
-setText("vrb", vrb);
-setText("concat", userMessage);
 
 onEvent("button2", "click", function () {
-  setText("plNoun", "");
+  setText("plural", "");
   setText("nn1", "");
   setText("nn2", "");
   setText("sng", "");
